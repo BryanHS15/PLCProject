a5d3760a6e50f5cca5025aa2603530208372ba48
BryanHS15 <73728125+BryanHS15@users.noreply.github.com>
2023-02-20 20:26:49 -0500

    Creater parser class and certain functions

---
+++ src/edu/ufl/cise/plcsp23/Parser.java
@@ -0,0 +1,20 @@
+package edu.ufl.cise.plcsp23;
+import edu.ufl.cise.plcsp23.ast.*;
+import edu.ufl.cise.plcsp23.ast.NumLitExpr;
+
+import java.util.List;
+
+public class Parser implements IParser  {
+    Scanner scanner;
+    private int current = 0;
+
+    Parser(Scanner input) {
+        scanner = input;
+    }
+    public AST parse() throws  PLCException {
+        Token t = (Token) scanner.next();
+        Token current = t;
+        NumLitExpr n = new NumLitExpr(current);
+        return n;
+    }
+}
