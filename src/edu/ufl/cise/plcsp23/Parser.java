ba69d4710534a28e99b9f68d17c7a205f1bf0f1e
SammyMasri2 <sammymasri@ufl.edu>
2023-02-21 12:56:36 -0500

    Wrote most of the functions

--- src/edu/ufl/cise/plcsp23/Parser.java
+++ src/edu/ufl/cise/plcsp23/Parser.java
@@ -10,7 +10,13 @@ public class Parser implements IParser  {
     private int currentIndex = 0;
     public ArrayList<Token> tokenList;
 
-
+    public AST parse() throws PLCException {
+        char[] tokens = scanner.inputChars;
+        Token nextToken = (Token) scanner.next();
+        Token current = nextToken;
+        NumLitExpr nxp = new NumLitExpr(current);
+        return nxp;
+    }
 
    public Parser(Scanner input) throws LexicalException{
         this.scanner = input;
@@ -26,6 +32,18 @@ public class Parser implements IParser  {
 
     }
 
+    private Expr expression() throws  PLCException{
+        if(tokenList.isEmpty()){
+            throw new PLCException("Error in expression");
+        }
+        if(match(IToken.Kind.RES_if)){
+            return conditional_expr();
+        }
+        else{
+            return or_expr();
+        }
+
+    }
 
     private Expr equality() {
         Expr expr = comparison();
@@ -73,44 +91,116 @@ public class Parser implements IParser  {
         return tokenList.get(currentIndex - 1);
     }
 
+    private Token consume (IToken.Kind kind) throws SyntaxException{
+        if(check(kind)){
+            return advance();
+        }
+        throw new SyntaxException("Consume error");
+
+    }
+
+    // <conditional_expr>  ::= if <expr> ? <expr> ? <expr>
+    private Expr conditional_expr() throws PLCException{
+        Expr left_expr = additive_expr();
+        IToken first_token = peek();
+
+        while(match(IToken.Kind.OR, IToken.Kind.BITOR)) {
+            Token operator = previous();
+            Expr right_expr = power_expr();
+            left_expr = new BinaryExpr(first_token, left_expr, operator.getKind(), right_expr);
+        }
+        return left_expr;
+
+    }
+
+    //<and_expr> ::=  <comparison_expr> ( ( & | && )  <comparison_expr>)*
+    private Expr and_expr() throws PLCException{
+        Expr left_expr = additive_expr();
+        IToken first_token = peek();
+
+        while(match(IToken.Kind.AND, IToken.Kind.BITAND)) {
+            Token operator = previous();
+            Expr right_expr = power_expr();
+            left_expr = new BinaryExpr(first_token, left_expr, operator.getKind(), right_expr);
+        }
+        return left_expr;
+
+    }
+
+    // <or_expr> ::=  <and_expr> (  ( | | || ) <and_expr>)*
+
+    private Expr or_expr() throws PLCException{
+        Expr left_expr = and_expr();
+        IToken first_token = peek();
+
+        while(match(IToken.Kind.OR, IToken.Kind.BITOR)) {
+            Token operator = previous();
+            Expr right_expr = power_expr();
+            left_expr = new BinaryExpr(first_token, left_expr, operator.getKind(), right_expr);
+        }
+        return left_expr;
+
+    }
+
+    //<power_expr> ::=    <additive_expr> ** <power_expr> |  <additive_expr>
+    private Expr power_expr() {
+       Expr left_expr = additive_expr();
+       IToken first_token = peek();
+
+       while(match(IToken.Kind.EXP)) {
+           Token operator = previous();
+           Expr right_expr = power_expr();
+           left_expr = new BinaryExpr(first_token, left_expr, operator.getKind(), right_expr);
+       }
+       return left_expr;
+
+    }
+
+
+    //<comparison_expr> ::=   <power_expr> ( (< | > | == | <= | >=) <power_expr>)*
     private Expr comparison() {
-        Expr expr = term();
+        Expr left_expr = power_expr();
+        Token first_token = peek();
 
         while (match(IToken.Kind.GT, IToken.Kind.GE, IToken.Kind.LT, IToken.Kind.LE)) {
             Token operator = previous();
-            Expr right = term();
-            expr = new BinaryExpr(expr, operator.getKind(), right);
+            Expr right_expr = power_expr();
+            left_expr = new BinaryExpr(first_token,left_expr, operator.getKind(), right_expr);
         }
 
-        return expr;
+        return left_expr;
     }
 
 
-    private Expr term() {
-        Expr expr = factor();
+    //<additive_expr> ::=  <multiplicative_expr> ( ( + | - ) <multiplicative_expr> )*
+    private Expr additive_expr() {
+        Expr left_expr = multiplicative_expr();
+        Token first_token = peek();
 
         while (match(IToken.Kind.MINUS, IToken.Kind.PLUS)) {
             Token operator = previous();
-            Expr right = factor();
-            expr = new BinaryExpr(expr, operator, right);
+            Expr right_expr = additive_expr();
+            left_expr = new BinaryExpr(first_token, left_expr, operator.getKind(), right_expr);
         }
 
-        return expr;
+        return left_expr;
     }
 
+
+    //<multiplicative_expr> ::= <unary_expr> (( * | / | % ) <unary_expr>)*
     private Expr multiplicative_expr() {
-        Expr expr = unary();
-        Token left = peek();
+        Expr left_expr = unary();
+        Token first_token = peek();
         while (match(IToken.Kind.DIV, IToken.Kind.TIMES, IToken.Kind.MOD)) {
             Token operator = previous();
-            Expr right = unary();
-            expr = new BinaryExpr(expr,operator, right);
+            Expr right_expr = unary();
+            left_expr = new BinaryExpr(first_token, left_expr, operator.getKind(), right_expr);
         }
 
-        return expr;
+        return left_expr;
     }
 
-
+//<unary_expr> ::= ( ! | - | sin | cos | atan) <unary_expr> |   <primary_expr>
     private Expr unary() {
         if (match(IToken.Kind.BANG, IToken.Kind.MINUS, IToken.Kind.RES_atan, IToken.Kind.RES_cos, IToken.Kind.RES_sin)) {
             Token first = peek();
@@ -122,6 +212,13 @@ public class Parser implements IParser  {
         return primary();
     }
 
+    //<primary_expr> ::=
+    //STRING_LIT |
+    //NUM_LIT |
+    //IDENT |
+    //( <expr> ) |
+    //Z |
+    //rand
     private Expr primary() {
         if (match(FALSE)) return new Expr.Literal(false);
         if (match(TRUE)) return new Expr.Literal(true);
@@ -141,11 +238,5 @@ public class Parser implements IParser  {
 
 
 
-    public AST parse() throws PLCException {
-        char[] tokens = scanner.inputChars;
-        Token nextToken = (Token) scanner.next();
-        Token current = nextToken;
-        NumLitExpr nxp = new NumLitExpr(current);
-        return nxp;
-    }
+
 }
