04fe61be558fec68e59aea72332d14c6511759e3
SammyMasri2 <sammymasri@ufl.edu>
2023-03-06 19:09:42 -0500

    Assignment 3 starter code

--- src/edu/ufl/cise/plcsp23/Scanner.java
+++ src/edu/ufl/cise/plcsp23/Scanner.java
@@ -55,7 +55,9 @@ public class Scanner implements  IScanner {
         reservedWords.put("atan", IToken.Kind.RES_atan);
         reservedWords.put("if", IToken.Kind.RES_if);
         reservedWords.put("while", IToken.Kind.RES_while);
-
+        reservedWords.put("red", IToken.Kind.RES_red);
+        reservedWords.put("blue", IToken.Kind.RES_blu);
+        reservedWords.put("green", IToken.Kind.RES_grn);
 
     }
 
